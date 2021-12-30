4de549506228182055669f82c1de06fc8e6af329
ashishshettyb1993 <ashishshettyb1993@gmail.com>
2021-12-30 19:12:19 +0900

    first commit

---
+++ src/test/java/com/example/testOpenShift/TestOpenShiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testOpenShift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestOpenShiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
